0976c1cb4c0cd0ead8d16ad6f0fc45be8126fbd9
Ramon@JUANDAVID <jdramon30@gmail.com>
2025-04-11 00:23:22 -0500

    Pay Mode View

---
+++ Models/PayModeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+namespace Supermarket_mvp.Models
+{
+    internal class PayModeModel
+    {
+        [DisplayName]
+       public int Id { get; set; }
+     public string Name { get; set; }
+     public string Observation { get; set; }
+    
+    }
+}
